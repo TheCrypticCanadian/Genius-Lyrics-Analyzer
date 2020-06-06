3a9647f37d52d5a4a31100f98be499203c4424d9
The Cryptic Canadian <ecrann@protonmail.com>
2020-06-06 10:40:24 -0230

    Added ability to search for albums

--- lyrics.py
+++ lyrics.py
@@ -2,13 +2,15 @@ import lyricsgenius
 import spotipy
 import constants
 from spotipy.oauth2 import SpotifyClientCredentials
-import pprint
+from pprint import pprint
 
 if __name__ == "__main__":
     genius = lyricsgenius.Genius(constants.GENIUS_ACCESS_TOKEN)
 
-    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+    sp = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials(constants.SPOTIPY_CLIENT_ID,constants.SPOTIPY_CLIENT_SECRET))
 
-    results = sp.search(q='weezer', limit=20)
-    for idx, track in enumerate(results['tracks']['items']):
-        print(idx, track['name'])
+    results = sp.search(q='Atrocity Exhibition', type="album", limit=20)
+    
+    for idx, album in enumerate(results['albums']['items']):
+        #pprint(album)
+        print(idx, album['name'], album['artists'][0]['name'])
